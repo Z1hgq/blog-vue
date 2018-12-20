ecc4620260831e26c0203c0a0875d5e1f73943ee
Z1hgq <1134348143@qq.com>
2018-12-20 21:47:14 +0800

    debug

--- src/views/admin/main.vue
+++ src/views/admin/main.vue
@@ -6,20 +6,20 @@
                 <Icon type="ios-analytics" />
                 博客管理
             </template>
-            <router-link to="/dist/admin/articalup"><MenuItem name="1-1">文章上传</MenuItem></router-link>
-            <router-link to="/dist/admin/articaladmin"><MenuItem name="1-2">文章管理</MenuItem></router-link>
-            <router-link to="/dist/admin/articalcadmin"><MenuItem name="1-3">分类管理</MenuItem></router-link>
+            <router-link to="/admin/articalup"><MenuItem name="1-1">文章上传</MenuItem></router-link>
+            <router-link to="/admin/articaladmin"><MenuItem name="1-2">文章管理</MenuItem></router-link>
+            <router-link to="/admin/articalcadmin"><MenuItem name="1-3">分类管理</MenuItem></router-link>
         </Submenu>
         <Submenu name="2">
             <template slot="title">
                 <Icon type="ios-filing" />
                 摄影管理
             </template>
-            <router-link to="/dist/admin/photoup"><MenuItem name="2-1">作品上传</MenuItem></router-link>
-            <router-link to="/dist/admin/photoadmin"><MenuItem name="2-2">作品管理</MenuItem></router-link>
-            <router-link to="/dist/admin/photocadmin"><MenuItem name="2-3">分类管理</MenuItem></router-link>
+            <router-link to="/admin/photoup"><MenuItem name="2-1">作品上传</MenuItem></router-link>
+            <router-link to="/admin/photoadmin"><MenuItem name="2-2">作品管理</MenuItem></router-link>
+            <router-link to="/admin/photocadmin"><MenuItem name="2-3">分类管理</MenuItem></router-link>
         </Submenu>
-        <router-link to="/dist/admin/about"><MenuItem name="3">
+        <router-link to="/admin/about"><MenuItem name="3">
             <Icon type="ios-cog" />
             关于
         </MenuItem></router-link>
