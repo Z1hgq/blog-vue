0eef7a22a43a92d34445d5b2bc6f76970e64b5d2
Z1hgq <z11hgq@gmail.com>
2019-09-30 17:55:34 +0800

    pro: 'http://127.0.0.1:3000'

--- src/config/index.js
+++ src/config/index.js
@@ -18,7 +18,7 @@ export default {
      */
     baseUrl: {
         dev: 'http://127.0.0.1:3000',
-        pro: 'http://api.cnarthub.com'
+        pro: 'http://127.0.0.1:3000'
     },
     /**
      * @description 默认打开的首页的路由name值，默认为home
